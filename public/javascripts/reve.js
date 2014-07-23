8550e43b7a1960d6ad03d253cbd0ad8ca34faa06
ambs <ambs@cpan.org>
2014-07-23 14:19:24 +0100

    Make hide/show revisions

--- public/javascripts/reve.js
+++ public/javascripts/reve.js
@@ -1,4 +1,16 @@
 
+function esconder_revisoes() {
+	$(".revisoes").hide();
+}
+
+function toggle_revisoes(button) {
+	$(".revisoes").toggle();
+	if ($(".revisoes").hasClass("hide")) {
+		button.html("Mostrar Revisões");
+	} else {
+		button.html("Esconder Revisões");
+	}
+}
 
 function obtain_user_name () {
 	var username = null;
--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rsync -aSPvz . --exclude ".git" --exclude "config.yml" --exclude "db/reve.sqlite" asimoes@dinis2:playground/reve/ 
+rsync -aSPvz . --exclude ".git" --exclude "config.yml" --exclude "db/reve.sqlite" asimoes@linguateca:Reve/ 
 
 ssh asimoes@dinis2 "chmod -R a+rwxt playground/reve/db"
